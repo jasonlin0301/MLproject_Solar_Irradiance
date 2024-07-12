7c76594526e89f15b491bddb12e855e1c965b195
Lanvis Wei <lanvis.wei@gmail.com>
2024-07-12 16:18:28 +0800

    modify

---
+++ temp_solar/statistics.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+# 加載 CSV 文件
+file_path = r'C:\Users\lanvi\OneDrive\Documents\github\MLproject_Solar_Irradiance\temp_solar\processed_data_v2.csv'
+
+# 字體設置
+font_path = r'C:\Users\lanvi\OneDrive\Documents\github\MLproject_Solar_Irradiance\ChocolateClassicalSans-Regular.ttf'
+font_properties = FontProperties(fname=font_path)
+
+df = pd.read_csv(file_path)
+
+# 定義需要計算的欄位
+columns_to_analyze = ['平均氣溫', '絕對最高氣溫', '絕對最低氣溫', '總日照時數h', '總日射量MJ/ m2']
+
+# 確保這些列中的數據都是數字
+for column in columns_to_analyze:
+    df[column] = pd.to_numeric(df[column], errors='coerce')
+
+# 去除離散值和異常值（使用IQR法）
+for column in columns_to_analyze:
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    df = df[~((df[column] < (Q1 - 1.5 * IQR)) | (df[column] > (Q3 + 1.5 * IQR)))]
+
+# 初始化統計結果字典
+stats_dict = {}
+describe_dict = {}
+
+# 計算每個欄位的統計量
+for column in columns_to_analyze:
+    d = df[column].dropna()
+    stats = {
+        '計數': d.count(),
+        '最小值': d.min(),
+        '最大值': d.max(),
+        '最小值索引': d.idxmin(),
+        '最大值索引': d.idxmax(),
+        '10%分位數': d.quantile(0.1),
+        '總和': d.sum(),
+        '均值': d.mean(),
+        '中位數': d.median(),
+        '眾數': d.mode().tolist(),  # 眾數可以有多個值
+        '方差': d.var(),
+        '標準差': d.std(),
+        '偏度': d.skew(),
+        '峰度': d.kurt()
+    }
+    stats_dict[column] = stats
+    describe_dict[column] = d.describe().to_dict()
+
+# 將統計數據轉換為 DataFrame
+stats_df = pd.DataFrame(stats_dict)
+describe_df = pd.DataFrame(describe_dict)
+
+# 創建圖表以顯示統計數據
+fig, ax = plt.subplots(figsize=(18, 12))
+ax.axis('tight')
+ax.axis('off')
+table = ax.table(cellText=stats_df.values, colLabels=stats_df.columns, rowLabels=stats_df.index, cellLoc='center', loc='center')
+table.auto_set_font_size(False)
+table.set_fontsize(12)
+table.scale(1.8, 1.8)
+plt.title('統計摘要', fontsize=20, fontproperties=font_properties)
+
+# 設置表格內文字體
+for key, cell in table.get_celld().items():
+    cell.set_text_props(fontproperties=font_properties)
+
+# 顯示統計數據圖表
+plt.show()
+
+# 創建圖表以顯示 describe 數據
+fig, ax = plt.subplots(figsize=(18, 12))
+ax.axis('tight')
+ax.axis('off')
+table = ax.table(cellText=describe_df.values, colLabels=describe_df.columns, rowLabels=describe_df.index, cellLoc='center', loc='center')
+table.auto_set_font_size(False)
+table.set_fontsize(12)
+table.scale(1.8, 1.8)
+plt.title('描述性統計摘要', fontsize=20, fontproperties=font_properties)
+
+# 設置表格內文字體
+for key, cell in table.get_celld().items():
+    cell.set_text_props(fontproperties=font_properties)
+
+# 顯示 describe 數據圖表
+plt.show()
